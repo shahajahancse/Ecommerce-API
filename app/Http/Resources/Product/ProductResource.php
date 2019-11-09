727e21b0e647876946f501d2b3b530199cee659b
Shahajahan15 <mdrezaali1@gmail.com>
2019-11-09 22:10:27 +0600

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,9 +18,14 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->details,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'discount' => $this->discount,
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 2),  // price show after discount
             'status' => $this->status,
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',  // rating show
+            'href' => [
+                'reviews' => route('reviews.index', $this->id) //spesific product check to review
+            ]
         ];
     }
 }
