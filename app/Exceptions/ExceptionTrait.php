cd301a7c83138d9ff88990bbef75aa090c493f86
Shahajahan15 <mdrezaali1@gmail.com>
2019-11-12 01:46:39 +0600

    Customized exception Trait

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,47 @@
+<?php 
+namespace App\Exceptions;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+trait ExceptionTrait
+{
+	public function apiException($request, $e)
+	{
+		if ($this->isModel($e)) {
+            return $this->ModelResponse($e);
+        }
+
+        if ($this->isHttp($e)) {
+            return $this->HttpResponse($e);
+        }
+	}
+
+
+	protected function isModel($e)
+	{
+		return $e instanceof ModelNotFoundException;
+	}
+
+	protected function isHttp($e)
+	{
+		return $e instanceof NotFoundHttpException;
+	}
+
+	protected function ModelResponse($e)
+	{
+		return response([
+                'error' => 'Invalid request url'
+            ], Response::HTTP_NOT_FOUND);
+	}
+
+	protected function HttpResponse($e)
+	{
+		return response([
+                'error' => 'Model Product not fond'
+            ], Response::HTTP_NOT_FOUND);
+	}
+}
+
+?>
